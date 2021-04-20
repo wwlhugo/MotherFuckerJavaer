3caf996d7c4bf66aa6e47721ecf4d11bdd2777a7
wwlhugo <wwlhugo@163.com>
2021-04-20 22:40:19 +0800

    修改了yaml文件,添加的了app名称属性
    添加了一个处理/hello请求的Controller,直接返回内容到界面

--- SpringBoot/src/main/java/com/mfj/springboot/SpringbootApplication.java
+++ SpringBoot/src/main/java/com/mfj/springboot/SpringbootApplication.java
@@ -3,6 +3,9 @@ package com.mfj.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 魏文龙
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
